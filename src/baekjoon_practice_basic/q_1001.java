2f4ad50d02bb1ff1b956e7abba0f1b24dcca904d
songhyun820 <a2208882@naver.com>
2023-03-19 23:19:58 +0900

    second commit

---
+++ src/baekjoon_practice_basic/q_1001.java
@@ -0,0 +1,14 @@
+package baekjoon_practice_basic;
+
+import java.util.Scanner;
+
+public class q_1001 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int a, b;
+		a = in.nextInt();
+		b = in.nextInt();
+		System.out.println(a - b);
+	}
+
+}
