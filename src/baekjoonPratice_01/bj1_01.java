101d393de1ffdb7250b1cb0926e8b9be1c74c967
songhyun820 <a2208882@naver.com>
2023-03-19 22:47:24 +0900

    second commit

---
+++ src/baekjoonPratice_01/bj1_01.java
@@ -0,0 +1,10 @@
+package baekjoonPratice_01;
+
+public class bj1_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author a2208
+ *
+ */
+module baekjoonPratice_01 {
+}
