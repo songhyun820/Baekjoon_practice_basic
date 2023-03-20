b95df5a2a6f3b6cacbfe0cca95e21c474ee45c8e
Song Hyun <songhyun@gachon.ac.kr>
2023-03-20 23:37:27 +0900

    plus

---
+++ src/baekjoon_practice_basic/q_10926.java
@@ -0,0 +1,11 @@
+package baekjoon_practice_basic;
+
+import java.util.Scanner;
+
+public class q_10926 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String name = in.nextLine();
+		System.out.println(name + "??!");		
+	}
+}
---
+++ src/baekjoon_practice_basic/q_10998.java
@@ -0,0 +1,13 @@
+package baekjoon_practice_basic;
+
+import java.util.Scanner;
+
+public class q_10998 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int a, b;
+		a = in.nextInt();
+		b = in.nextInt();
+		System.out.println(a * b);	
+	}
+}
