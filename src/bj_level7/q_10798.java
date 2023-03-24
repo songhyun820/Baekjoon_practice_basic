f1377ae8dcd49e10c0187f37b101581b13d8faf2
songhyun8820 <a2208882@naver.com>
2023-03-24 18:02:44 +0900

    a

--- src/bj_level7/q_10798.java
+++ src/bj_level7/q_10798.java
@@ -1,5 +1,24 @@
 package bj_level7;
 
-public class q_10798 {
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class q_10798 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		char words[][] = new char[5][15];
+		String result = "";
+		for(int i = 0; i < 5; i++) {
+			String str = sc.nextLine();
+			for(int j = 0; j < str.length(); j++) {
+				words[i][j] = str.charAt(j);
+			}
+		}
+		for(int i = 0; i < 15; i++) {
+			for(int j = 0; j < 5; j++) {
+				result += words[j][i];
+			}
+		}
+		System.out.println(result);
+	}
 }
