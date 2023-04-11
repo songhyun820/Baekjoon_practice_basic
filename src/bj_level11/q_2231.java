d32d8f32e9f3b1fd5b1d98b41e29ede90c2cc613
Song Hyun <songhyun@gachon.ac.kr>
2023-04-11 21:53:37 +0900

    sloving lev11

---
+++ src/bj_level11/q_2231.java
@@ -0,0 +1,17 @@
+package bj_level11;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class q_2231 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String number = br.readLine();
+		int input = Integer.parseInt(number);
+		int sum = 0;
+		int result = 0;
+		
+	}
+
+}
--- src/bj_level11/q_2798.java
+++ src/bj_level11/q_2798.java
@@ -1,5 +1,31 @@
 package bj_level11;
 
-public class q_2798 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class q_2798 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer str = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(str.nextToken());
+		int M = Integer.parseInt(str.nextToken());
+		int card[] = new int[n];
+		int max = 0;
+		StringTokenizer str2 = new StringTokenizer(br.readLine());
+		for(int i = 0;  i < n; i++) {
+			card[i] = Integer.parseInt(str2.nextToken());
+		}
+		for(int a = 0; a < n-2; a++) {
+			for(int b = a+1; b< n-1; b++) {
+				for(int c = b+1; c< n; c++) {
+					int sum = card[a]+card[b]+card[c];
+					if( sum <= M && sum > max)
+						max = sum;
+				}
+			}
+		}
+		System.out.println(max);		
+	}
 }
